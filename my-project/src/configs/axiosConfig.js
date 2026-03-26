b850d33f16ff641ad77a24626c763dfc6ca08c32
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-03-26 15:12:22 +0530

    done

--- my-project/src/configs/axiosConfig.js
+++ my-project/src/configs/axiosConfig.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://rujal-x-adarsh-backend.onrender.com/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
